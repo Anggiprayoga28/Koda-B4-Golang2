782574efbe40f336aa05a6ef956ec5944683cca9
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-22 00:35:03 +0700

    Fix: Add logic to insert 88 before value 66 in scores slice

--- main.go
+++ main.go
@@ -3,11 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	scores := []int{50, 75, 66, 20, 32, 90}
+	scores := []int{50, 75, 20, 32, 66, 90}
 
-	scores = append(scores[:3], append([]int{88}, scores[3:]...)...)
-
+	index66 := 0
 	for i := 0; i < len(scores); i++ {
-		fmt.Println(scores[i])
+		if scores[i] == 66 {
+			index66 = i
+			break
+		}
+	}
+
+	scoresBaru := []int{}
+	for i := 0; i < index66; i++ {
+		scoresBaru = append(scoresBaru, scores[i])
+	}
+	scoresBaru = append(scoresBaru, 88)
+	for i := index66; i < len(scores); i++ {
+		scoresBaru = append(scoresBaru, scores[i])
+	}
+
+	for i := 0; i < len(scoresBaru); i++ {
+		fmt.Println(scoresBaru[i])
 	}
 }
